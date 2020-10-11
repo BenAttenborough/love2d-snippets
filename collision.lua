0d6d41dd0a788a77d2b21f1f8670d7dd97f1d18a
WebDeveloperBen <battenborough@gmail.com>
2020-10-11 20:05:35 +0100

    Generalised collision detection

--- collision.lua
+++ collision.lua
@@ -1,6 +1,6 @@
 function Player:collide(obstacle)
-    if (self.x + 2) + (self.width - 4) >= pipe.x and self.x + 2 <= pipe.x + PIPE_WIDTH then
-        if (self.y + 2) + (self.height - 4) >= pipe.y and self.y + 2 <= pipe.y + PIPE_HEIGHT then
+    if (self.x + 2) + (self.width - 4) >= obstacle.x and self.x + 2 <= obstacle.x + obstacle.width then
+        if (self.y + 2) + (self.height - 4) >= obstacle.y and self.y + 2 <= obstacle.y + obstacle.height then
             return true
         end
     end
